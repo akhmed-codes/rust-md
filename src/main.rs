b01f8f92d2bde7a9468d209a56a82b91a3794e39
akhmed-codes <cubicsstack@gmail.com>
2021-09-08 16:42:18 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+use std::env;
+use std::fs;
+use std::process;
+
+fn get_filename(args: Vec<String>) -> String {
+    if args.len() < 2 {
+        println!("Expected file name");
+        process::exit(1);
+    }
+    fs::read_to_string(&args[1]).expect("Expected file name")
+}
+
+fn main() {
+    let file_contents = get_filename(env::args().collect());
+    // println!("{}", file_contents);
+
+    let one_liner_chunks_vec = file_contents.lines().collect::<Vec<&str>>();
+    // println!("{:?}", one_liner_chunks_vec);
+
+    for (i, e) in one_liner_chunks_vec.iter().enumerate() {
+        // let plusone = one_liner_chunks_vec[i];
+        
+        let word_chunks: Vec<&str> = e.split_whitespace().collect();
+        let characterized_chunks: Vec<char> = e.trim().chars().collect();
+
+        if characterized_chunks.len() == 0 {
+            continue;
+        }
+        let first_word = word_chunks[0];
+        let _top_level_parse = match first_word {
+            "#" => println!("<h1>{}</h1>", e.replacen(first_word, "", 1).trim()),
+            "##" => println!("<h2>{}</h2>", e.replacen(first_word, "", 1).trim()),
+            "###" => println!("<h3>{}</h3>", e.replacen(first_word, "", 1).trim()),
+            ">" => {
+                //check if one space is present after '>'
+                if characterized_chunks[1] == ' ' {
+                    println!(
+                        "<blockquote>{}</blockquote>",
+                        e.replacen(first_word, "", 1).trim()
+                    )
+                } else {
+                    println!("{}", e.replacen(first_word, "", 1).trim())
+                }
+            }
+            _ => {
+                let formatting = if one_liner_chunks_vec[i] == "" { 
+                    println!("")
+                }
+                else if i == 0 && one_liner_chunks_vec[i + 1] != "" {
+                    println!("<p>{}", e)
+                } else if i == 0 && one_liner_chunks_vec[i + 1] == "" {
+                    println!("<p>{}</p>", e)
+                } else if i == one_liner_chunks_vec.len() - 1 && one_liner_chunks_vec[i - 1] != "" {
+                    println!("{}</p>", e)
+                } else if i == one_liner_chunks_vec.len() - 1 && one_liner_chunks_vec[i - 1] == "" {
+                    println!("<p>{}</p>", e)
+                } else if one_liner_chunks_vec[i - 1] == "" && one_liner_chunks_vec[i + 1] == "" {
+                    println!("<p>{}<p>", e)
+                } else if one_liner_chunks_vec[i - 1] == "" && one_liner_chunks_vec[i + 1] != "" {
+                    println!("<p>{}", e)
+                } else if one_liner_chunks_vec[i - 1] != "" && one_liner_chunks_vec[i + 1] != "" {
+                    println!("{}", e)
+                } else {
+                    println!("{}</p>", e)
+                };
+                formatting
+            }
+        };
+        // println!("{}", top_level_parse);
+    }
+}
+
